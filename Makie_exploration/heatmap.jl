47f2a666c53beecd4adc209ac9f2c868b223c57c
arthurto <arthur.ps22009@gmail.com>
2022-02-20 16:28:23 -0300

    +mandelbrot

---
+++ Makie_exploration/heatmap.jl
@@ -0,0 +1,13 @@
+using CairoMakie
+
+function mandelbrot(x, y)
+    z = c = x + y*im
+    for i in 1:30.0; abs(z) > 2 && return i; z = z^2 + c; end; 0
+end
+
+#fig = Figure(resolution = (800, 600))
+
+fog = heatmap(-2:0.002:1, -1.1:0.002:1.1, mandelbrot,
+    colormap = Reverse(:deep),resolution = (3200, 2400))
+
+save("mandelbrot.png",fog, pt_per_unit = 1)
