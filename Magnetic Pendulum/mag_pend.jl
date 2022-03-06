c92e06271651f5ab34462591ac34232efbb2b11a
arthurto <arthur.ps22009@gmail.com>
2022-03-06 15:09:53 -0300

    animation finished

--- Magnetic Pendulum/mag_pend.jl
+++ Magnetic Pendulum/mag_pend.jl
@@ -38,16 +38,43 @@ function f(r,p,t)::SVector
     return SVector(rx,rxx,ry,ryy)
 end
 
-ds = ContinuousDynamicalSystem(f,[1.,0.,1.,0.],[0.5,0.5,0.1])
+ds = ContinuousDynamicalSystem(f,[1.,0.,1.,0.],[0.05,0.45,0.05])
 
-tr = trajectory(ds, 10.0,u = [1.,0.,1.,0.] ,Δt = 0.01)
+#tr = trajectory(ds, 10.0,u = [1.,1.,1.,0.] ,Δt = 0.1)
+#tr = trajectory(ds, 20.0,[rand(1)[1],0.,rand(1)[1],0.])
+tr = trajectory(ds, 50.0,[1.,0.,1.3,0.])
+tr1 = trajectory(ds, 50.0,[1.,0.,1.2,0.])
+tr2 = trajectory(ds, 50.0,[1.,0.,1.1,0.])
+
 
 # defining the figure
 begin 
-
-    plot(xlims=(-2,2),ylim=(-2,2),size = (600,600))
-    scatter!([m1[1]],[m1[2]])
-    scatter!([m2[1]],[m2[2]])
-    scatter!([m3[1]],[m3[2]])
-    plot!(tr[:,1],tr[:,3])
+    plot(xlims=(-2,2),ylim=(-2,2),size = (600,600),axis=nothing
+    , legend=false,foreground_color_subplot=colorant"white")
+    plot!(tr[:,1],tr[:,3],lw=2,color=2)
+    plot!(tr1[:,1],tr1[:,3],lw=2,color=3)
+    plot!(tr2[:,1],tr2[:,3],lw=2,color=1)
+    scatter!([m1[1]],[m1[2]],ms=10,color =1)
+    scatter!([m2[1]],[m2[2]],ms=10,color =2)
+    scatter!([m3[1]],[m3[2]],ms=10,color =3)
 end
+
+begin 
+
+    plot(xlims=(-2,2),ylim=(-2,2),size = (600,600),axis=nothing
+    , legend=false,foreground_color_subplot=colorant"white")
+
+    anim = @animate for i in 1:25:length(tr[:,1])
+        plot!(tr[:,1],tr[:,3],lw=2,color=2)
+    plot!(tr1[:,1],tr1[:,3],lw=2,color=3)
+    plot!(tr2[:,1],tr2[:,3],lw=2,color=1)
+    scatter!([m1[1]],[m1[2]],ms=10,color =1)
+    scatter!([m2[1]],[m2[2]],ms=10,color =2)
+    scatter!([m3[1]],[m3[2]],ms=10,color =3)
+    end
+
+    gif(anim,"anim_mag_pend2.gif",fps=30)
+
+end
+
+5001/240
